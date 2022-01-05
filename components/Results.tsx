924bcdd3fe97c32c2ea5172c51110d877c339b0d
NRotondaro <niksonrotondaro1@gmail.com>
2022-01-05 17:17:06 -0300

    feat(ts): adding typescript to the project with errors

--- components/Results.tsx
+++ components/Results.tsx
@@ -1,7 +1,11 @@
 import Thumbnail from './Thumbnail';
 import FlipMove from 'react-flip-move';
 
-export default function Results({ results }) {
+interface ResultProps {
+  results: 
+}
+
+export default function Results({ results }: ResultProps) {
   return (
     <FlipMove className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center'>
       {results.map((result) => (
