505d8d95dba3850ec4c3fb24b7c925ddae60afad
NRotondaro <niksonrotondaro1@gmail.com>
2022-01-05 21:40:22 -0300

    fix(ts): type errors on Thumbnail and Results

--- components/Results.tsx
+++ components/Results.tsx
@@ -1,11 +1,11 @@
 import Thumbnail from './Thumbnail';
 import FlipMove from 'react-flip-move';
 
-interface ResultProps {
-  results: JSON;
+interface ResultsProps {
+  results: [{}];
 }
 
-export default function Results({ results }: ResultProps) {
+export default function Results({ results }) {
   return (
     <FlipMove className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center'>
       {results.map((result) => (
--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -8,50 +8,52 @@ interface ThumbnailProps {
     poster_path: string;
     overview: string;
     title: string;
-    original_name: string; 
+    original_name: string;
     media_type: string;
     release_date: string;
     first_air_date: string;
     vote_count: string;
-  }
+  };
 }
 
-const Thumbnail = forwardRef(({ result }: ThumbnailProps, ref) => {
-  const BASE_URL = 'https://image.tmdb.org/t/p/original/';
+const Thumbnail = forwardRef(
+  ({ result }: ThumbnailProps, ref: React.Ref<HTMLDivElement>) => {
+    const BASE_URL = 'https://image.tmdb.org/t/p/original/';
 
-  return (
-    <div
-      ref={ref}
-      className='p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50'
-    >
-      <Image
-        layout='intrinsic'
-        src={
-          `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
-          `${BASE_URL}${result.poster_path}`
-        }
-        height={1080}
-        width={1920}
-        alt='thumbnail'
-      />
-      <div className='p-2'>
-        <p className='truncate max-w-md'>{result.overview}</p>
+    return (
+      <div
+        ref={ref}
+        className='p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50'
+      >
+        <Image
+          layout='intrinsic'
+          src={
+            `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
+            `${BASE_URL}${result.poster_path}`
+          }
+          height={1080}
+          width={1920}
+          alt='thumbnail'
+        />
+        <div className='p-2'>
+          <p className='truncate max-w-md'>{result.overview}</p>
 
-        <h2 className='mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold'>
-          {result.title || result.original_name}
-        </h2>
+          <h2 className='mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold'>
+            {result.title || result.original_name}
+          </h2>
 
-        <div>
-          <p className='flex items-center opacity-0 group-hover:opacity-100'>
-            {result.media_type && `${result.media_type} •`}{' '}
-            {result.release_date || result.first_air_date} •{' '}
-            <ThumbUpIcon className='h-5 mx-2' /> {result.vote_count}
-          </p>
+          <div>
+            <p className='flex items-center opacity-0 group-hover:opacity-100'>
+              {result.media_type && `${result.media_type} •`}{' '}
+              {result.release_date || result.first_air_date} •{' '}
+              <ThumbUpIcon className='h-5 mx-2' /> {result.vote_count}
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  );
-});
+    );
+  }
+);
 
 Thumbnail.displayName = 'Thumbnail';
 
